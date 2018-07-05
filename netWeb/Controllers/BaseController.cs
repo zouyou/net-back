3e0a5efd8357ae63a636647ab6da7c3e5fa08bf5
邹世友 <zoushy@yonyou.com>
2018-07-05 11:45:05 +0800

    初始化项目

---
+++ netWeb/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace netWeb.Controllers
+{
+    public abstract class BaseController : ApiController
+    {
+        [HttpGet, Route("findOne")]
+        public string findOne(string id)
+        {
+            return "QQ:511570155";
+        }
+    }
+}
---
+++ netWeb/Controllers/UserInfoController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace netWeb.Controllers
+{
+    /// <summary>
+    /// 用户接口
+    /// </summary>
+    [RoutePrefix("v1/user")]
+    public class UserInfoController : BaseController
+    {
+    }
+}
